c01b5dcbe681beaa0085d381f0acf14fa5b9a062
Xiang <xiangli.cs@gmail.com>
2017-06-25 10:53:44 -0700

    add reporting

---
+++ reporting/prometheus.go
@@ -0,0 +1,27 @@
+package reporting
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type PrometheusReporter struct {
+	stateGauge prometheus.Gauge
+}
+
+func (pr *PrometheusReporter) ReportState(s int) {
+	pr.stateGauge.Set(float64(s))
+}
+
+func NewPrometheus(proberName string) *PrometheusReporter {
+	sg := prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: "kprober",
+			Subsystem: proberName,
+			Name:      "state",
+			Help:      "The state of the prober.",
+		})
+
+	return &PrometheusReporter{
+		stateGauge: sg,
+	}
+}
