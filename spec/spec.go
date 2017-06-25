567836ed53ae751b971bee07852813a2880adec7
Xiang <xiangli.cs@gmail.com>
2017-06-25 11:20:11 -0700

    add spec

---
+++ spec/spec.go
@@ -0,0 +1,30 @@
+package spec
+
+type ProberSpec struct {
+	Name string
+	Target
+	Probe
+}
+
+// Only one of the field should be set.
+type Target struct {
+	Pod *string
+}
+
+// Only one of the field should be set.
+type Probe struct {
+	HTTP *HTTPProbe
+	Ping *PingProbe
+}
+
+type HTTPProbe struct {
+	Method string // Only Get and Head are supported currently
+	Port   string
+	Path   string
+
+	StatusRegex *string
+	BodyRegex   *string
+	LineMatch   *string
+}
+
+type PingProbe struct{}
