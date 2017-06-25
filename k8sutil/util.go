59da8e8a02aaea0baf5bc4c10274c92655734ca5
Xiang <xiangli.cs@gmail.com>
2017-06-25 11:14:32 -0700

    add k8sutil

---
+++ k8sutil/util.go
@@ -0,0 +1,18 @@
+package k8sutil
+
+func IPFromPod() string {
+	return ""
+}
+
+func IPFromService() string {
+	return ""
+}
+
+func IPsFromReplicaSet() []string {
+	return nil
+
+}
+
+func IPsFromDeployments() []string {
+	return nil
+}
